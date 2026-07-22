d1ae4d9d3b535bf36fea60888f15cd3a4955bf76
sd <sd@COMLAB2-PC28>
2026-07-22 13:22:39 +0300

    identity added.

---
+++ Rassef/Models/Identity/Group.cs
@@ -0,0 +1,13 @@
+﻿namespace Rassef.Models.Identity
+{
+    public class Group : BaseEntity
+    {
+        public string Name { get; set; }
+        public ICollection<User> Users { get; set; } 
+            = new HashSet<User>();
+
+        public ICollection<GroupPermission> GroupPermissions { get; set; }
+            = new HashSet<GroupPermission>();
+
+    }
+}
--- Rassef/Models/Identity/User.cs
+++ Rassef/Models/Identity/User.cs
@@ -1,7 +1,13 @@
-﻿namespace Rassef.Models.Identity
-{
-    public class User
-    {
+﻿using Azure.Core.Pipeline;
 
+namespace Rassef.Models.Identity
+{
+    public class User : BaseEntity
+    {
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }    
+        public Guid groupId { get; set; }
+        public Group group { get; set; }
     }
 }
