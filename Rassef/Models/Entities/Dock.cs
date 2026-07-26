fca2aa5ecf0eac96f59a5b3608d18227352351d3
MennaYouse <mennayousef141@gmail.com>
2026-07-26 11:16:37 +0300

    Update navigation properties in Dock model

--- Rassef/Models/Entities/Dock.cs
+++ Rassef/Models/Entities/Dock.cs
@@ -7,7 +7,6 @@
         public Guid DockStatusId { get; set; }
         public User CreatedBy { get; set; } = null!;
         public Department Department { get; set; }
-        public Warehouse Warehouse { get; set; }
         public Dock_statuses DockStatus { get; set; }
         public ICollection<DockAssignment> DockAssignments { get; set; }    
     }
