f81676353c85b36ea9b3292d4895e593c78535f4
sd <sd@COM174-LAB3>
2026-07-26 10:20:50 +0300

    ready

--- Rassef/Models/Identity/GroupPermission.cs
+++ Rassef/Models/Identity/GroupPermission.cs
@@ -1,11 +1,9 @@
-﻿
-
-namespace Rassef.Models.Identity
+﻿namespace Rassef.Models.Identity
 {
     public class GroupPermission
     {
         public Guid GroupId { get; set; }
-        public Group? Group { get; set; }
+        public GroupUser? Group { get; set; }
 
         public Guid PermissionId { get; set; }
         public Permissions? Permission { get; set; }
