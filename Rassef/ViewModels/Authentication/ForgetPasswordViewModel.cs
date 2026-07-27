57badd167911473601b1f7bdc0ec16a07a889d18
MennaYouse <mennayousef141@gmail.com>
2026-07-27 13:46:23 +0300

    ready

--- Rassef/ViewModels/Authentication/ForgetPasswordViewModel.cs
+++ Rassef/ViewModels/Authentication/ForgetPasswordViewModel.cs
@@ -2,8 +2,7 @@
 {
     public class ForgetPasswordViewModel
     {
-        [Required(ErrorMessage = "Email is required.")]
-        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+    
         [Display(Name = "Email Address")]
         public string Email { get; set; } = string.Empty;
     }
