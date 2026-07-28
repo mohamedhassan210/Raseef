361f19201883806a2c7e37a23238b441e0b8dbdf
MennaYouse <mennayousef141@gmail.com>
2026-07-28 10:12:26 +0300

    ready

---
+++ Rassef/ViewModels/Warehouse/CreateWarehouseVM.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Rassef.ViewModels.Warehouse
+{
+    public class CreateWarehouseVM
+    {
+            [Display(Name = "Warehouse Name")]
+            public string Name { get; set; } = string.Empty;
+
+            [Display(Name = "Location")]
+            public string Location { get; set; } = string.Empty;
+
+            public IEnumerable<SelectListItem>? Departments { get; set; }
+            public IEnumerable<SelectListItem>? Docks { get; set; }
+
+    }
+}
---
+++ Rassef/ViewModels/Warehouse/WarehouseListVM.cs
@@ -0,0 +1,12 @@
+﻿namespace Rassef.ViewModels.Warehouse
+{
+    public class WarehouseListVM
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public string CreatedByName { get; set; } = string.Empty;
+        public int DocksCount { get; set; }
+        public int DepartmentsCount { get; set; }
+    }
+}
