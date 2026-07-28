275b2b9ce340095b3440ddba8c22956c9b66f0be
MennaYouse <mennayousef141@gmail.com>
2026-07-28 11:12:10 +0300

    fix warehouse controller error

--- Rassef/Controllers/WarehouseController.cs
+++ Rassef/Controllers/WarehouseController.cs
@@ -10,7 +10,7 @@
         }
 
         [HttpGet]
-        public async Task<IActionResult> Index(CancellationToken cancellationToken)
+        public async Task<IActionResult> Index()
         {
             var warehouses = await _warehouseRepository.GetAllAsync();
             return View(warehouses);
@@ -70,8 +70,8 @@
         {
             if (id != warehouse.Id) return NotFound();
 
-            bool isUnique = await _warehouseRepository.IsNameUniqueAsync(warehouse.Name, excludedId: id);
-            if (!isUnique)
+            bool isUnique = await _warehouseRepository.IsNameUniqueAsync(warehouse.Name);
+                if (!isUnique)
             {
                 ModelState.AddModelError("Name", "اسم المستودع مستخدم بالفعل لمستودع آخر.");
             }
