9a1bf385f4ba45a8029d01566123a79fa0505dc3
Fares Mohammed <fares.mohammed.dev@gmail.com>
2026-07-28 14:43:14 +0300

    end of day commit

--- Rassef/Common/Interfaces/IWarehouseRepository.cs
+++ Rassef/Common/Interfaces/IWarehouseRepository.cs
@@ -3,15 +3,9 @@
 {
 
     public interface IWarehouseRepository : IRepository<Warehouse>
-
     {
-
         Task<Warehouse?> GetWithDetailsByIdAsync(Guid id);
-
-
-
         Task<bool> IsNameUniqueAsync(string name);
-
     }
 
 }
