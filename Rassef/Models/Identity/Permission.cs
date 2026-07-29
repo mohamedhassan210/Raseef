61bf1f14c4cd1be304fd24d74815bd907a746902
Fares Mohammed <fares.mohammed.dev@gmail.com>
2026-07-29 09:55:50 +0300

    ready to pull

--- Rassef/Models/Identity/Permission.cs
+++ Rassef/Models/Identity/Permission.cs
@@ -3,6 +3,7 @@
     public class Permission : BaseEntity
     {
         public string Name { get; set; }
+        public string? Description { get; set; }
         public ICollection<GroupPermission> GroupPermissions { get; set; }
             = new HashSet<GroupPermission>();
     }
