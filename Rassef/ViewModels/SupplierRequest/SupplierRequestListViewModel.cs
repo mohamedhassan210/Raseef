1951271d5fff7914d93856e98a7a46d5cc332b18
Fares Mohammed <fares.mohammed.dev@gmail.com>
2026-07-29 14:09:08 +0300

    add custom SupplierRequestRepository with eager loading and refactor controller

---
+++ Rassef/ViewModels/SupplierRequest/SupplierRequestListViewModel.cs
@@ -0,0 +1,28 @@
+﻿public class SupplierRequestListVM
+{
+    public int Id { get; set; }
+
+    [Display(Name = "المورد")]
+    public string SupplierName { get; set; } = string.Empty;
+
+    [Display(Name = "رقم لوحة الشاحنة")]
+    public string TruckPlateNumber { get; set; } = string.Empty;
+
+    [Display(Name = "اسم السائق")]
+    public string DriverName { get; set; } = string.Empty;
+
+    [Display(Name = "رقم هاتف السائق")]
+    public string DriverPhone { get; set; } = string.Empty;
+
+    [Display(Name = "القسم")]
+    public string DepartmentName { get; set; } = string.Empty;
+
+    [Display(Name = "حالة الطلب")]
+    public string RequestStatusName { get; set; } = string.Empty;
+
+    [Display(Name = "رقم التصريح")]
+    public string PermitNumber { get; set; } = string.Empty;
+
+    [Display(Name = "مواد غذائية؟")]
+    public bool IsFood { get; set; }
+}
