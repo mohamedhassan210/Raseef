88acd6ba4f7b3fa49269bd24a2dff34fa607e079
sd <sd@COM182-LAB3>
2026-08-11 09:35:03 +0300

    add the new migration

--- Rassef/Controllers/AdministrationController.cs
+++ Rassef/Controllers/AdministrationController.cs
@@ -1,13 +1,5 @@
-﻿using DocumentFormat.OpenXml.Spreadsheet;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Rassef.Common.Interfaces.Services.AuthenticationServices;
-using Rassef.Models.Entities;
-using Rassef.Models.Identity;
-using Rassef.Models.ValueObjects;
-using Rassef.ViewModels.Administration;
+﻿using Rassef.ViewModels.Administration;
 using Rassef.ViewModels.Administration.Employee;
-using Rassef.ViewModels.Administration.Truck;
 namespace Rassef.Controllers
 {
     public class AdministrationController : Controller
@@ -28,7 +20,7 @@ namespace Rassef.Controllers
             _truckRepository = truckRepository;
             _truckTypesRepository = truckTypeRepository;
 
-            
+
         }
         //Employee Administration
         [HttpGet]
