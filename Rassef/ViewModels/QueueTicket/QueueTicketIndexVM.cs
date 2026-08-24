d80cea58971803c8f44cb53d49ca0f01f987f6c1
Fares Mohammed <fares.mohammed.dev@gmail.com>
2026-08-24 10:11:17 +0300

    Implement exact UI design for LiveQueue display board matching TV screen design

--- Rassef/ViewModels/QueueTicket/QueueTicketIndexVM.cs
+++ Rassef/ViewModels/QueueTicket/QueueTicketIndexVM.cs
@@ -1,8 +1,19 @@
-﻿namespace Rassef.ViewModels.QueueTicket
+namespace Rassef.ViewModels.QueueTicket
 {
+    public class LiveDockCardVM
+    {
+        public string DockLetter { get; set; } = "A";
+        public string DockName { get; set; } = "رصيف 5";
+        public string CurrentTicketNumber { get; set; } = "A265";
+        public string NextTicketNumber { get; set; } = "A266";
+        public string Status { get; set; } = "جاري";
+    }
+
     public class QueueTicketIndexVM
     {
         public List<QueueTicketListVM> Tickets { get; set; } = new();
+
+        public List<LiveDockCardVM> DockCards { get; set; } = new();
 
         public int CompletedCount { get; set; }
 
